7e0e3375e9b0ea3d3bfb043239f44e454f4d0003
jakehasgithubnow <jacob.murphy@zenjob.com>
2025-08-12 18:22:22 +0200

    s

--- app/server/routes/admin/stylePrompts.js
+++ app/server/routes/admin/stylePrompts.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import * as stylePrompts from '../../../db/stylePrompts.js';
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
 // Get all prompts
 router.get('/', async (req, res) => {
