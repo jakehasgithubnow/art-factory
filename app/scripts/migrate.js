6ba7e2d1b757a7101afae174db431935e0f19495
jakehasgithubnow <jacob.murphy@zenjob.com>
2025-08-16 09:42:33 +0200

    w

--- app/scripts/migrate.js
+++ app/scripts/migrate.js
@@ -2,14 +2,10 @@
 import fs from 'node:fs/promises';
 import pg from 'pg';
 
-const { DATABASE_URL } = process.env;
-if (!DATABASE_URL) {
-  console.error('DATABASE_URL is not set');
-  process.exit(1);
-}
+import { env } from '../config/env.js';
 
-const sql = await fs.readFile(new URL('../app/db/schema.sql', import.meta.url), 'utf8');
-const client = new pg.Client({ connectionString: DATABASE_URL, ssl: { rejectUnauthorized: false } });
+const sql = await fs.readFile(new URL('../db/schema.sql', import.meta.url), 'utf8');
+const client = new pg.Client({ connectionString: env.databaseUrl });
 await client.connect();
 try {
   await client.query(sql);
